126b65cad9242c4989013f6eeaefdfc3d94bd87e
lenovo <lenovo@DESKTOP-OBJUSFO>
2021-01-23 18:25:29 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> sehirler = new MyDictionary<string, string>();
+
+            sehirler.Add("06", "Ankara");
+            sehirler.Add("34", "İstanbul");
+            sehirler.Add("35", "İzmir");
+
+            Console.WriteLine(sehirler);
+
+        }
+    }
+
+}
